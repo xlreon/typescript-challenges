24255b01415c5d15d23f2b2c58ffb4e786bb4549
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2021-02-17 10:44:42 +0530

    MyPick | typescript

---
+++ pick.ts
@@ -0,0 +1,16 @@
+type MyPick<T, K extends keyof T> = {
+    [X in K]: T[X]
+}
+
+interface Todo {
+    title: string
+    description: string
+    completed: boolean
+  }
+  
+  type TodoPreview = MyPick<Todo, 'title' | 'completed'>
+  
+  const todo: TodoPreview = {
+      title: 'Clean room',
+      completed: false,
+  }
